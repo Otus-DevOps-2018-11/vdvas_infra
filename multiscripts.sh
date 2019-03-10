7e01fc4aae73b9c0c3e36b1afead4bc6942eecc0
Dmitriy Vasiliev <vdvas2@gmail.com>
2019-03-10 19:33:52 +0300

    Delete multiscripts.sh

--- multiscripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gsutil cp  gs://scripts-2018-11/install_ruby.sh ~
-gsutil cp  gs://scripts-2018-11/install_mongodb.sh ~
-gsutil cp  gs://scripts-2018-11/deploy.sh ~
-chmod +x *.sh
-./install_ruby.sh
-./install_mongodb.sh
-./deploy.sh
